90cf4192afda39380d0684fc64148c3c39fe3cb0
mikael <mikael.st15@protonmail.com>
2025-11-14 19:41:28 -0300

    feat: adding feature to save last page read in a comic

--- src/pages/Reader/index.tsx
+++ src/pages/Reader/index.tsx
@@ -5,7 +5,7 @@ import { Screen } from "../../components/Screen";
 import { Actions, ChangePage, Infos, Main } from "./style";
 import { NextBtn } from "../../components/buttons/NextBtn";
 import { PrevBtn } from "../../components/buttons/PrevBtn";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useLiveQuery } from "dexie-react-hooks";
 import { database } from "../../service/db/db";
 import { useEffect, useState } from "react";
@@ -13,17 +13,36 @@ import { extractPages } from "../../service/utils/extractPages";
 import { ReaderView } from "./ReaderView";
 import { formatNumber } from "../../service/utils/formatNumber";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
+import type { ReadingComic } from "../../service/models/Reading";
 
 export function Reader() {
   const navigate = useNavigate();
 
   const { comickey } = useParams();
+  const ckey = decodeURIComponent(comickey ?? '');
 
-  const comic = useLiveQuery(() => database.comics.where('key').equals(decodeURIComponent(comickey ?? '')).first());
+  const comic = useLiveQuery(() => database.comics.where('key').equals(ckey).first());
+  const readingComic = useLiveQuery(() => database.reading.where('comicKey').equals(ckey).first())
 
   const [pages, setPages] = useState<Blob[]>([]);
   const [actualPage, setActualPage] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
+
+  const onCloseReader = async () => {
+    navigate('..');
+
+    if (readingComic) {
+      await database.reading.update(readingComic.id, { actualPage: actualPage })
+    } else {
+      const reading: ReadingComic = {
+        comicId: comic?.id ?? NaN,
+        comicKey: comic?.key ?? '',
+        actualPage: actualPage
+      }
+
+      await database.reading.add(reading);
+    }
+  }
 
   const toNextPage = () => {
     if (actualPage < pages.length - 1) {
@@ -57,7 +76,9 @@ export function Reader() {
     setLoading(true);
     if (!comic) return;
 
-    console.log(comickey);
+    if (readingComic) {
+      setActualPage(readingComic.actualPage)
+    };
 
     fetchPages();
   }, [comic])
@@ -66,7 +87,7 @@ export function Reader() {
     return (
       <>
         <Infos>
-          <button onClick={() => navigate('..')}>
+          <button onClick={onCloseReader}>
             <ChevronLeftIcon width={32} />
           </button>
           <ComicInfos
---
+++ src/service/models/Reading.ts
@@ -0,0 +1,6 @@
+export interface ReadingComic {
+  id?: number;
+  comicId: number;
+  comicKey: string;
+  actualPage: number;
+}

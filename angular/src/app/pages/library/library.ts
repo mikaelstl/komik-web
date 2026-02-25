758d5daa4e91dad70c11b9c844548607ad9d3604
mikael <mikael.st15@protonmail.com>
2026-02-25 10:07:49 -0300

    feat: finished library page

--- angular/src/app/pages/library/library.ts
+++ angular/src/app/pages/library/library.ts
@@ -3,7 +3,7 @@ import { AppBar } from '../../components/toolbars/app-bar/app-bar';
 import { ComicVCard } from '../../components/cards/comic-vcard/comic-vcard';
 import { Title } from "../../components/base/title/title";
 import { NgIcon, provideIcons } from "@ng-icons/core";
-import { heroBookmarkSolid } from '@ng-icons/heroicons/solid';
+import { heroBookmarkSolid, heroChevronDownSolid, heroChevronUpSolid } from '@ng-icons/heroicons/solid';
 import { HighlightCaption } from "../../components/base/highlight-caption/highlight-caption";
 
 @Component({
@@ -17,12 +17,14 @@ import { HighlightCaption } from "../../components/base/highlight-caption/highli
 ],
   providers: [
     provideIcons({
-      heroBookmarkSolid
+      heroBookmarkSolid,
+      heroChevronDownSolid,
+      heroChevronUpSolid
     })
   ],
   templateUrl: './library.html',
   styleUrl: './library.scss',
 })
 export class LibraryPage {
-  comics: number[] = Array.from({length: 8}, (_, i) => i);
+  comics: number[] = Array.from({length: 12}, (_, i) => i);
 }
